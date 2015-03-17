6e286d67dc9bd1289b84b4210fc38b20487935aa
bakerz <de1719@163.com>
2015-03-17 19:55:55 +0800

    add login register css

--- static/js/start/index.js
+++ static/js/start/index.js
@@ -1,21 +1,38 @@
-$(function(){
-	onFun();
-});
-
-function onFun(){
-	$('.register').on('click', function(){
-		$(this).addClass('active');
-		$('.login').removeClass('active');
-		$('.form-register').css('display','block');
-		$('.form-login').css('display','none');
-	});
-	$('.login').on('click', function(){
-		$(this).addClass('active');
-		$('.register').removeClass('active')
-		$('.form-login').css('display','block');
-		$('.form-register').css('display','none');
-	});
-	$('.login-btn').on('click', function(){
-		//location.href="/simpleBlog/2015/03/08/courses.html";
-	})
-};
+var indexPage = (function() {
+    var show = true,
+    end;
+    
+    function onFun(){
+        $('.register').on('click', function(){
+            $(this).addClass('active');
+            $('.login').removeClass('active');
+            $('.form-register').css('display','block');
+            $('.form-login').css('display','none');
+        });
+        $('.login').on('click', function(){
+            $(this).addClass('active');
+            $('.register').removeClass('active')
+            $('.form-login').css('display','block');
+            $('.form-register').css('display','none');
+        });
+        $('.login-btn').on('click', function(){
+            //location.href="/simpleBlog/2015/03/08/courses.html";
+        })
+    };
+    
+    function showCursor() {
+        if(show){
+            $(".console-panel span").css("color","#fff");
+            show = false;
+        }else{
+            $(".console-panel span").css("color","#000");
+            show = true;
+        }
+        setTimeout(showCursor, 500);
+    };
+    
+    onFun();
+    showCursor();
+    
+    return {};
+})();
